5d4fcd21d0e4966313fb7f1ba29b74b8b52fd3ab
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2020-07-19 16:51:08 -0700

    Add files via upload

---
+++ assignment3/cmake-build-debug/CMakeFiles/assignment3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment3.dir/emalloc.c.o"
+  "CMakeFiles/assignment3.dir/list.c.o"
+  "CMakeFiles/assignment3.dir/senjify3.c.o"
+  "assignment3.pdb"
+  "assignment3.exe"
+  "libassignment3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
